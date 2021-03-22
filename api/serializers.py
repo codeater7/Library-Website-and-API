4f31ec7d58abc4622532ebd0a6358e7984b15510
Sujan <pokharelsujan134@gmail.com>
2021-03-22 22:25:45 +0200

    Serilizers commented

--- api/serializers.py
+++ api/serializers.py
@@ -7,3 +7,9 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ('title', 'subtitle', 'author', 'isbn')
+
+# if the app will be not have its own  database model , there is no need to create own file. 
+
+# Serilizers, translate data into a format that is easy to consume over the internet, typically JSON
+
+# From which model ,what things?? 
